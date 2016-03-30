7d0f5c09522234b6acca1ea0ae3f0490c1027e29
Teyras <Teyras@gmail.com>
2016-03-30 12:56:54 +0200

    Add documentation of config adapters

--- src/cz/cuni/mff/ConfigMapper/ConfigAdapter.java
+++ src/cz/cuni/mff/ConfigMapper/ConfigAdapter.java
@@ -4,10 +4,20 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Created by teyras on 28.3.16.
+ * Provides reading and writing functionality for a configuration file format
  */
 public interface ConfigAdapter {
+    /**
+     * Parse an input into a ConfigNode structure
+     * @param input The input stream
+     * @return The parsed tree
+     */
     ConfigNode read(InputStream input);
 
+    /**
+     * Write a ConfigNode structure into an output stream
+     * @param config The configuration structure
+     * @param output The output stream
+     */
     void write(ConfigNode config, OutputStream output);
 }
--- src/cz/cuni/mff/ConfigMapper/IniAdapter.java
+++ src/cz/cuni/mff/ConfigMapper/IniAdapter.java
@@ -4,14 +4,24 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Created by teyras on 28.3.16.
+ * A configuration adapter for INI files
  */
 public class IniAdapter implements ConfigAdapter {
+    /**
+     * Parse config from an INI file
+     * @param input The input stream
+     * @return The configuration structure
+     */
     @Override
     public ConfigNode read(InputStream input) {
         return null;
     }
 
+    /**
+     * Write config into an INI file
+     * @param config The configuration structure
+     * @param output The output stream
+     */
     @Override
     public void write(ConfigNode config, OutputStream output) {
 
