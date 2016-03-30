96464df15ca762154af050d268c6848ea1d4bc10
Teyras <Teyras@gmail.com>
2016-03-30 13:04:02 +0200

    Add LoadingMode doc

--- src/cz/cuni/mff/ConfigMapper/LoadingMode.java
+++ src/cz/cuni/mff/ConfigMapper/LoadingMode.java
@@ -1,9 +1,16 @@
 package cz.cuni.mff.ConfigMapper;
 
 /**
- * Created by teyras on 29.3.16.
+ * Represents the way of mapping configuration to objects
  */
 public enum LoadingMode {
+    /**
+     * Any undeclared fields result in an error
+     */
     STRICT,
+
+    /**
+     * Undeclared options are stored in a field annotated with {@link UndeclaredOptions}
+     */
     RELAXED
 }
