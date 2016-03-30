93dc4f57a4044262441eb72959956bfeee26f892
Teyras <Teyras@gmail.com>
2016-03-30 13:33:01 +0200

    Add ConfigMapper doc

--- src/cz/cuni/mff/ConfigMapper/ConfigMapper.java
+++ src/cz/cuni/mff/ConfigMapper/ConfigMapper.java
@@ -1,16 +1,28 @@
 package cz.cuni.mff.ConfigMapper;
 
 /**
- * Created by teyras on 28.3.16.
+ * Maps {@link ConfigNode} structures to objects
  */
 public class ConfigMapper<MappedObject> {
 
+    /**
+     * Class of mapped objects
+     */
     private Class<MappedObject> cls;
 
+    /**
+     * @param cls Class the mapper should map config objects to
+     */
     public ConfigMapper(Class<MappedObject> cls) {
         this.cls = cls;
     }
 
+    /**
+     * Map config to a newly created instance of MappedObject
+     * @param config The configuration tree to be mapped
+     * @param mode The mapping mode
+     * @return A new instance of MappedObject with options from config
+     */
     public<MappedObject> MappedObject load(ConfigNode config, LoadingMode mode) {
         try {
             return (MappedObject) cls.newInstance();
@@ -23,6 +35,12 @@ public class ConfigMapper<MappedObject> {
         return null;
     }
 
+    /**
+     * Store mapped options from a MappedObject instance to an existing config structure.
+     * Any config nodes not affected by the values in the mapped object remain the same.
+     * @param object The source instance
+     * @param config The config structure into which the mapped options are stored
+     */
     public void save(MappedObject object, ConfigNode config) {
 
     }
