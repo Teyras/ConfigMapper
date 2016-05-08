e4e1bcf607465fec21c382b30ca37c329236637a
Teyras <teyras@gmail.com>
2016-05-08 14:29:25 +0200

    add a cause constructor to exceptions

--- src/cz/cuni/mff/ConfigMapper/MappingException.java
+++ src/cz/cuni/mff/ConfigMapper/MappingException.java
@@ -7,4 +7,8 @@ public class MappingException extends Exception {
 	public MappingException(String message) {
 		super(message);
 	}
+
+	public MappingException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
